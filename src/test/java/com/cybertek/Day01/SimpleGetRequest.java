c2db1806cf60404af00b45b30a9fa91f00b6e4bb
ozanpolat48 <mrozanpolat@gmail.com>
2022-08-16 11:41:39 +0300

    Initial commit

---
+++ src/test/java/com/cybertek/Day01/SimpleGetRequest.java
@@ -0,0 +1,26 @@
+package com.cybertek.Day01;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Test;
+
+public class SimpleGetRequest {
+
+
+    String url = "http://3.83.131.158:8000/api/spartans";
+
+    @Test
+    public void test1(){
+
+        //send a get request and save response inside the Response object
+        Response response = RestAssured.get(url);
+
+        //print response status code
+        System.out.println(response.statusCode());
+
+        //print response body
+        response.prettyPrint();
+
+    }
+
+}
