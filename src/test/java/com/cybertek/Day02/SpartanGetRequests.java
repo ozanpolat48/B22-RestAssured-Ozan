9825e869a010f76ea6499ac183b24a615b9ad713
ozanpolat48 <mrozanpolat@gmail.com>
2022-08-18 14:27:30 +0300

    ready for first testcase

--- src/test/java/com/cybertek/Day02/SpartanGetRequests.java
+++ src/test/java/com/cybertek/Day02/SpartanGetRequests.java
@@ -4,9 +4,9 @@ public class SpartanGetRequests {
 
     String url = "http://3.83.131.158:8000/api/spartans";
 
-    //Given Accept type application/xml
+    //Given Accept type application/json
     //When user send GET request to api/spartans end point
-    //Then response Content Type must be application/xml
+    //Then response Content Type must be application/json
     //And response body should be xml format
 
 }
