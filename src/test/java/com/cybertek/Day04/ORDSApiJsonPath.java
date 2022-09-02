6c9778860a0c0982c7242414238d85e0d20b6ec5
ozanpolat48 <mrozanpolat@gmail.com>
2022-09-02 14:30:32 +0300

    Day04: ORDSApiWithJson.java

---
+++ src/test/java/com/cybertek/Day04/ORDSApiJsonPath.java
@@ -0,0 +1,25 @@
+package com.cybertek.Day04;
+
+import com.cybertek.utilities.HRTestBase;
+import com.cybertek.utilities.HRTestBase;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ORDSApiJsonPath extends HRTestBase {
+
+    @DisplayName("GET request to Countries")
+    @Test
+    public void test1(){
+
+
+
+    }
+}
