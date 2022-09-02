b727ace160bc30e150b74aa6ca0a20715025cd38
ozanpolat48 <mrozanpolat@gmail.com>
2022-09-02 13:13:24 +0300

    Day04: SpartanWithJson Class

---
+++ src/test/java/com/cybertek/Day04/SpartanWithJsonPath.java
@@ -0,0 +1,21 @@
+package com.cybertek.Day04;
+
+import com.cybertek.utilities.SpartanTestBase;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SpartanWithJsonPath extends SpartanTestBase {
+
+    @DisplayName("GET one spartan with JsonPath")
+    @Test
+    public void test1(){
+
+
+
+    }
+}
